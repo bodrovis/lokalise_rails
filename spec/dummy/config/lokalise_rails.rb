d7a2d3a3408965f2260f4767219c84726a58bcdf
bodrovis <bodrovis@protonmail.com>
2023-12-22 19:46:48 +0200

    bump

--- spec/dummy/config/lokalise_rails.rb
+++ spec/dummy/config/lokalise_rails.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 if defined?(LokaliseRails) && defined?(LokaliseRails::GlobalConfig)
   LokaliseRails::GlobalConfig.config do |c|
-    c.api_token = ENV['LOKALISE_API_TOKEN']
-    c.project_id = ENV['LOKALISE_PROJECT_ID']
-end
+    c.api_token = ENV.fetch('LOKALISE_API_TOKEN', nil)
+    c.project_id = ENV.fetch('LOKALISE_PROJECT_ID', nil)
+  end
 end
